c9f741eeb17d88f787a8865298020d7b7001949e
aksm <albert.min@gmail.com>
2016-09-07 13:02:52 -0400

    Working game function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,176 @@
 $(document).ready(function() {
-	var apicall = "https://crossorigin.me/http://jservice.io/api/random?count=26";
+
+	// Game variables
+	// Append to api call before pushing to heroku: https://crossorigin.me/
+	var apicall = "https://crossorigin.me/http://jservice.io/api/random?count=100";
 	var parsedData = "";
 	var question = "";
+	var qcounter = 0;
+	var answers = [];
+	var correct = 0;
+	var wrong = 0;
+	var introAudio = $("#intro-audio")[0];
+	var timerAudio = $("#timer-audio")[0];
+	var buzzerAudio = $("#buzzer-audio")[0];
+	var dingAudio = $("#ding-audio")[0];
+	var timerCount = 13;
+	var timerInterval = "";
+	var answer = "";
+	var rounds = 1;
+	
+	// Array shuffling function to randomize answers
+	function shuffle(array) {
+  		var currentIndex = array.length, temporaryValue, randomIndex;
 
-	$.getJSON(apicall, function(data){
+  		// While there remain elements to shuffle...
+  		while (0 !== currentIndex) {
 
-    	parsedData = JSON.parse(JSON.stringify(data));
-    	// console.log(parsedData);
-    	
-		question = parsedData[0].question;
-	// console.log(question);
-    $("#question").html("<h2>"+question+"</h2>");
+	    	// Pick a remaining element...
+		    randomIndex = Math.floor(Math.random() * currentIndex);
+		    currentIndex -= 1;
 
+		    // And swap it with the current element.
+		    temporaryValue = array[currentIndex];
+		    array[currentIndex] = array[randomIndex];
+		    array[randomIndex] = temporaryValue;
+		}
+
+  		return array;
+	}
+
+	// Set up question and answers and call timer function.
+	function startQ() {
+		console.log(rounds);
+		if(rounds == 14) {
+			hide($(".hide"));
+			show($(".transition"));
+			$("#transition-text").html("<h1>GAME OVER.</h1><p>CORRECT ANSWERS: "+correct+"</p><p>INCORRECT ANSWERS: "+wrong+"</p><p><button class='start-button'>Restart Game</button></p>");
+		} else if (rounds < 14) {
+    		$("#transition-text").empty();
+    		show($(".hide"));
+			$("html").css("background-image", "url('assets/images/jasontrebek.gif')");
+	    	qcounter+=4;		
+			answers = [];
+			answer = "";
+			clue = parsedData[qcounter];
+			$("header").css({"margin-top": "50px", "text-align": "left", "font-size": "75px", "padding-left": "20px"});
+			$(".gameplay").css("display", "inline-block");
+			$("#category").html("<h2>"+clue.category.title.toUpperCase()+"</h2>");
+			$("#question").html("<h2>"+clue.question.toUpperCase()+"</h2>");
+			$("#answers").empty();
+			for(i=qcounter; i<qcounter+4; i++) {
+				answers.push(parsedData[i].answer.toUpperCase());
+			}
+			shuffle(answers);
+			$.each(answers, function(f, v) {
+				$("#answers").append("<p><button class='answer'>"+v+"</button></p>");
+			});
+
+			timer();
+			// Insane work-around to strip out api accidental html tags
+			answer = $("<p>").html(clue.answer).text();
+			console.log(clue.answer);
+			console.log(answer);
+    		rounds++;
+		}
+	}
+
+	// Timer function
+	function timer() {
+		clearInterval(timerInterval);
+		timerCount = 13;
+		$(document).on('mousemove', function(e){
+			var x = e.pageX;
+			var y = e.pageY - 100;
+    		$('#time-remaining').css({
+       			left: x,
+       			top: y
+    		});
+		});
+   		timerAudio.currentTime = 0;
+		timerAudio.play();
+		timerInterval = setInterval(function() {
+			if (timerCount > 0) {
+				$("#time-remaining").html("TIME LEFT: "+timerCount);
+				timerCount--;			
+			} else if (timerCount == 0) {
+				timerAudio.pause();
+				buzzerAudio.play();
+    			$("html").css({"background-color": "#0039c4", "background-image": "none"});
+    			hide($(".hide"));
+    			show($(".transition"));
+    			$("#transition-text").html("<h1>TIME'S UP.</h1><p>The correct answer is: "+answer.toUpperCase()+"</p>")
+    			wrong++;
+    			clearInterval(timerInterval);
+    			setTimeout(startQ, 5000);
+			}
+		}, 1000);
+	}
+
+	// Hide elements for transitions
+	function hide(element) {
+		element.removeClass("display-block");
+		element.addClass("display-none");
+	}
+
+	// Show elements after transitions
+	function show(element) {
+		element.removeClass("display-none");
+		element.addClass("display-block");
+	}
+
+	// Click event for game start and restart
+	$("body").on("click", ".start-button", ".answer", function() {
+
+		// API call
+		$.getJSON(apicall, function(data){
+	    	parsedData = JSON.parse(JSON.stringify(data));
+	    	console.log(parsedData);
+	     		$("#transition-text").empty();
+				hide($(".transition"));
+				$(".intro").addClass("display-none");
+				introAudio.play();
+				setTimeout(function() {$("html").css("background-image", "url('assets/images/jasontrebek.gif')")}, 5000);
+				correct = 0;
+				wrong = 0;
+				qcounter = 0;
+				rounds = 1;
+				answer = "";
+				setTimeout(startQ, 10000);
+
+			// Click event for answer choices
+	    	$("body").on("click", ".answer", function(event) {
+	    		// Workaround for nested click event mess
+	    		event.stopImmediatePropagation();
+
+	    		// Check for correct answer
+		    	if(timerCount > 0) {
+		    		timerAudio.currentTime = 0;
+		    		timerAudio.pause();
+		    		if($(this).text() == answer.toUpperCase()) {
+			    		dingAudio.play();
+		    			$("html").css({"background-color": "#0039c4", "background-image": "none"});
+		    			hide($(".hide"));
+		    			show($(".transition"));
+		    			$("#transition-text").html("<h1>CORRECT!</h1>")
+		    			correct++;
+		    			console.log("Correct: "+correct);
+		    			clearInterval(timerInterval);
+		    			setTimeout(startQ, 5000);
+		    		} else if($(this).text() != answer.toUpperCase()) {
+		    			buzzerAudio.play();
+		    			$("html").css({"background-color": "#0039c4", "background-image": "none"});
+		    			hide($(".hide"));
+		    			show($(".transition"));
+		    			$("#transition-text").html("<h1>The correct answer is: </h1><p>"+answer.toUpperCase()+"</p>")
+		    			wrong++;
+		    			console.log("Wrong: "+wrong);
+		    			clearInterval(timerInterval);
+		    			setTimeout(startQ, 5000);
+		    		}
+	    		} 
+	    	});
+		});
     });
-	// for(i=0;i<parsedData.length;i++) {
-	// 	if(parsedData[i].invalid_count == !null) {
-			
-	// 	}
-	// }
+
 });
